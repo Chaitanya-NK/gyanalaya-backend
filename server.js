6859b27bc9dbaa185ac61750b2a6f6ddf2f5e281
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-12-05 00:29:33 +0530

    Fix: issues

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ import commentRoutes from "./routes/commentRoutes.js";
 import postCategoriesRoutes from "./routes/postCategoriesRoutes.js";
 import pdfRoutes from "./routes/pdfRoutes.js";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"
 
 dotenv.config();
 connectDB();
