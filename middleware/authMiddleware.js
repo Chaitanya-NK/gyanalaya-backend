3fb01405a93284605665a9bad1abb0ff4e3aafc8
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-12-05 00:22:05 +0530

    Fix: auth middleware issue

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,7 @@
-import { verify } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+
+const { verify } = jwt;
 
 export const authGuard = async (req, res, next) => {
   if (
@@ -12,12 +14,12 @@ export const authGuard = async (req, res, next) => {
       req.user = await User.findById(id).select("-password");
       next();
     } catch (error) {
-      let err = new Error("Not authorized, Token failed");
+      const err = new Error("Not authorized, Token failed");
       err.statusCode = 401;
       next(err);
     }
   } else {
-    let error = new Error("Not authorized, No token");
+    const error = new Error("Not authorized, No token");
     error.statusCode = 401;
     next(error);
   }
@@ -27,7 +29,7 @@ export const adminGuard = (req, res, next) => {
   if (req.user && req.user.admin) {
     next();
   } else {
-    let error = new Error("Not authorized as an admn");
+    const error = new Error("Not authorized as an admin");
     error.statusCode = 401;
     next(error);
   }
