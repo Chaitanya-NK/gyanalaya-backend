b9478d2510db2fcb30bdac06d3ec6e81418e4a0c
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-12-05 21:04:26 +0530

    Fix: Cors

--- server.js
+++ server.js
@@ -22,11 +22,12 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-const corsOptions = {
-  exposedHeaders: "*"
-};
-
-app.use(cors(corsOptions));
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+  next(); 
+})
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
