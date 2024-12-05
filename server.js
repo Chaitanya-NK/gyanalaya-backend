8439678fc2486981c6f66836b79ee657926b22b4
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-12-05 21:19:49 +0530

    Fix: Cors

--- server.js
+++ server.js
@@ -26,7 +26,28 @@ app.use(express.json());
 //   exposedHeaders: "*"
 // };
 
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://gyanalaya-blog.onrender.com/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
