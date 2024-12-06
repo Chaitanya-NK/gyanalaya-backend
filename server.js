7e1aa62b90d3409aa4fd7026a7140eb5dc7c8dff
Chaitanya-NK <chaitanya.nk2002@gmail.com>
2024-12-06 21:31:50 +0530

    Fix: cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 // CORS Configuration
 const corsOptions = {
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: "*",
   credentials: true,
   exposedHeaders: "*",
 };
